9c0621800ade8d71e729154ede3170344e32080e
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-04-03 14:54:57 +0800

    feat: implement ChannelAttention, SpatialAttention, and CBAM classes

--- src/model.py
+++ src/model.py
@@ -72,3 +72,48 @@ class DenseBlock(nn.Module):
 
 # ===================== CONVOLUTIONAL BLOCK ATTENTION MODULE (CBAM) =====================
 # TODO: create 3 classes (channel attention, spatial attention, and CBAM to integrate the channel and spatial attention to be used for the dense block)
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool3d(1)
+        self.max_pool = nn.AdaptiveMaxPool3d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(in_channels, in_channels // reduction_ratio, bias=False),
+            nn.ReLU(),
+            nn.Linear(in_channels // reduction_ratio, in_channels, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _, _ = x.shape
+
+        avg_out = self.fc(self.avg_pool(x).view(b, c))
+        max_out = self.fc(self.max_pool(x).view(b, c))
+        out = avg_out + max_out
+        out = out.view(b, c, 1, 1, 1)
+        return x * out
+    
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv = nn.Conv3d(2, 1, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+        self.bn = nn.BatchNorm3d(1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv(x)
+        return self.sigmoid(x)
+        
+class CBAM(nn.Module):
+    def __init__(self, in_channels):
+        super(CBAM, self).__init__()
+        self.ca = ChannelAttention(in_channels)
+        self.sa = SpatialAttention()
+
+    def forward(self, x):
+        x = x * self.ca(x)
+        x = x * self.sa(x)
+        return x
