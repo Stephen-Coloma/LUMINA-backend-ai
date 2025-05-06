b251688ee2e1bf098ddf892fd4f79aa52f19dc4b
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-05-07 00:45:03 +0800

    feat: implement FastAPI application with root endpoint and router integration

---
+++ agent/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from routers.app_routers import router
+
+app = FastAPI()
+
+app.get("/")
+def read_root():
+    return {
+        "message": "Welcome to the Backend API of the Agent",
+        "docs": "/docs",
+        "api_base": "/api"
+    }
+
+app.include_router(router, prefix="/api", tags=["api"])
