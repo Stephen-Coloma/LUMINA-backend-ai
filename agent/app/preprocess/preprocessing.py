9c552180c4881dc4721c130ef0d6b8c111fca624
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-05-07 21:36:40 +0800

    feat: add preprocessing functionality for patient DICOM data

--- agent/app/preprocess/preprocessing.py
+++ agent/app/preprocess/preprocessing.py
@@ -1,26 +1,15 @@
-from preprocess.ai.dicom_augmentor import DicomAugmentor
 from preprocess.ai.dicom_converter import DicomConverter
 from preprocess.ai.volume_processing import VolumeProcessor
 from preprocess.ai.intensity_processing import IntensityProcessor
-from preprocess.ai.roi_slice_filter import DicomROIFilter
-from preprocess.ai.dicom_converter import save_arrays
-
+from pathlib import Path
 
 # Define preprocessing for AI
-# TODO: Change the parameters to accomodate the new data structure
-def preprocess_patient_data(patient_dir: Path):
-    patient_dicom_ct = patient_dir / 'CT'
-    patient_dicom_pet = patient_dir / 'PET'
+async def preprocess_patient_data(ct_dir: Path, pet_dir: Path):
 
     try:
-
-        # Check if there are DICOM files in both the CT and PET directories after filtering
-        ct_files = list(patient_dicom_ct.glob('*.dcm'))
-        pet_files = list(patient_dicom_pet.glob('*.dcm'))
-
         # Convert slices to 2D NumPy arrays
-        ct_slices = DicomConverter().to_2d_array(patient_dicom_ct)
-        pet_slices = DicomConverter().to_2d_array(patient_dicom_pet)
+        ct_slices = DicomConverter().to_2d_array(ct_dir)
+        pet_slices = DicomConverter().to_2d_array(pet_dir)
 
         # Apply pixel intensity conversion and normalization
         ct_slices = IntensityProcessor(ct_slices, True).convert()
@@ -34,12 +23,11 @@ def preprocess_patient_data(patient_dir: Path):
         ct_volume = VolumeProcessor(ct_volume).resize_depth(15)
         pet_volume = VolumeProcessor(pet_volume).resize_depth(15)
 
-        return {
-            'ct_volume': ct_volume,
-            'pet_volume': pet_volume,
-        }
 
+        return ct_volume, pet_volume
 
     except Exception as e:
-        raise
+        raise RuntimeError("Preprocessing failed.") from e
+
+
 # Define preprocessing for Data Sci
