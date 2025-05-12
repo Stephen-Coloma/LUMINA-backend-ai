39783ac635664be08139aa83449259d67f0956a9
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-05-12 21:01:22 +0800

    refactor: update model loading paths to use os.path.join for better portability

--- agent/app/models/model.py
+++ agent/app/models/model.py
@@ -1,11 +1,14 @@
 import torch
 import joblib
+import os
 
 # AI Model
 gpu = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-ai_model = torch.jit.load("C:/Users/Renuel Balogo/Documents/Projects Cloned/nsclc-classifier-ai/agent/app/models/best_model.pt")
+ai_model_path = os.path.join("app", "models", "best_model.pt")
+ai_model = torch.jit.load(ai_model_path)
 ai_model = ai_model.to(gpu)
 ai_model.eval()
 
 # Machine Learning Model
-data_sci_model= joblib.load("C:/Users/Renuel Balogo/Documents/Projects Cloned/nsclc-classifier-ai/agent/app/models/logistic_model.pkl")
+ml_model_path = os.path.join("app", "models", "logistic_model.pkl")
+data_sci_model = joblib.load(ml_model_path)
